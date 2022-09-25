0b440d5e561f804aadb16d3992f5056739ce2c2b
Omikorin <michal.k2001@gmail.com>
2022-09-26 01:40:28 +0200

    format code

--- classes/Browser.js
+++ classes/Browser.js
@@ -3,25 +3,30 @@ import puppeteer from 'puppeteer';
 export default class Browser {
   constructor() {
     const waitUntil = process.env.WAIT_UNTIl;
+
     switch (waitUntil) {
       case '0': {
         this.wait = 'networkidle0';
         break;
       }
+
       case '1': {
         this.wait = 'networkidle2';
         break;
       }
+
       case '2': {
         this.wait = 'domcontentloaded';
         break;
       }
     }
   }
+
   async launch() {
     this.browser = await puppeteer.launch();
     this.ws = this.browser.wsEndpoint();
   }
+
   async render(url) {
     const start = Date.now();
     const fullUrl = `${process.env.SPA_URL}${url}`;
@@ -32,11 +37,15 @@ export default class Browser {
       await page.goto(fullUrl, {
         waitUntil: this.wait,
       });
+
       const time = Date.now() - start;
+
       console.info(`Rendered ${fullUrl} page in ${time}ms`);
+
       return page.evaluate(() => document.documentElement.outerHTML);
     } catch (e) {
       console.error(`[classes][Server][render] - ${e}`);
+
       return e;
     }
   }
