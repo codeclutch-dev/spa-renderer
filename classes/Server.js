27f947f50e6d12a06046f35bc4aaefb4f6a4ae07
Omikorin <michal.k2001@gmail.com>
2022-09-26 01:21:44 +0200

    format code

--- classes/Server.js
+++ classes/Server.js
@@ -1,37 +1,37 @@
-import express from "express";
-import Cache from "./Cache.js";
-import Browser from "./Browser.js";
+import express from 'express';
+import Cache from './Cache.js';
+import Browser from './Browser.js';
 
 const port = process.env.PORT || 5000;
 
 export default class Server {
-    constructor() {
-        this.useCache = process.env.USE_CACHE === 'true';
-        if(this.useCache) this.cache = new Cache();
-        this.browser = new Browser();
-    }
-    async start() {
-        this.app = express();
-        this.listener = this.app.listen(port, () => {
-            console.log(`Server listening on port: ${port}`);
-        })
-        await this.browser.launch();
-    }
-    listen() {
-        this.app.get('*', async (req, res) => {
-            const url = req.url;
-            if(this.cache && this.cache.has(url)) {
-                res.send(this.cache.get(url))
-                console.info(`${url} served from cache.`)
-            } else {
-                const html = await this.browser.render(req.url);
-                res.send(html);
-                console.info(`${url} served from renderer.`)
-                if(this.cache) this.cache.set(url, html);
-            }
-        })
-    }
-    stop() {
-        this.listener.close();
-    }
+  constructor() {
+    this.useCache = process.env.USE_CACHE === 'true';
+    if (this.useCache) this.cache = new Cache();
+    this.browser = new Browser();
+  }
+  async start() {
+    this.app = express();
+    this.listener = this.app.listen(port, () => {
+      console.log(`Server listening on port: ${port}`);
+    });
+    await this.browser.launch();
+  }
+  listen() {
+    this.app.get('*', async (req, res) => {
+      const url = req.url;
+      if (this.cache && this.cache.has(url)) {
+        res.send(this.cache.get(url));
+        console.info(`${url} served from cache.`);
+      } else {
+        const html = await this.browser.render(req.url);
+        res.send(html);
+        console.info(`${url} served from renderer.`);
+        if (this.cache) this.cache.set(url, html);
+      }
+    });
+  }
+  stop() {
+    this.listener.close();
+  }
 }
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import * as dotenv from 'dotenv';
-import Server from "./classes/Server.js";
+import Server from './classes/Server.js';
 
 dotenv.config();
 
 const server = new Server();
+
 server.start();
 server.listen();
-
